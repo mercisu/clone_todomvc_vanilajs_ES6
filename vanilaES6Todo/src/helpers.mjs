975cb610cfe78f5e0ddf871d621a5fa4914a7699
suyeon <suyeon.jeong@atto-research.com>
2020-04-07 11:09:22 +0900

    이벤트 위임과 call

--- vanilaES6Todo/src/helpers.mjs
+++ vanilaES6Todo/src/helpers.mjs
@@ -1,4 +1,32 @@
+/*
+이벤트 위임 함수 이다.
+target : 이벤트 위임한 상위 element
+selector : 이벤트 대상
+type: 이벤트 종류
 
+이벤트가 발생시 타겟과 선언한 target들 목록 중에서 일치하는 것이 있으면
+handler에 target 컨텍스트와 이벤트 객체를 전달한다.
+위 동작을 taget 이벤트 리스너 등록한다.
+* */
+
+export function $delegate(target, selector, type, handler, capture) {
+    const dispatchEvent = event => {
+        const targetElement = event.target;
+        const potentialElement = target.querySelectorAll(selector);
+        let i = potentialElement.length;
+
+        while(i--) {
+            if(potentialElement[i] === targetElement) {
+                handler.call(targetElement,event);
+                //call 함수는 첫번째 파라미터는 handler에 호출되는 this값 context를 넘긴다.
+                //call은 메소드를 작성하면 새객체를 위한 메소드를 재작성할 필요없이 다른 객체에 상속 할 수 있다.
+                break;
+            }
+        }
+    };
+
+    $on(target, type, dispatchEvent, !!capture);
+}
 
 export function qs(selector, scope) {
     return (scope || document).querySelector(selector);
